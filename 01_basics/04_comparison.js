c7b74ee0258547c7c05db160b5145a30aff3b87d
niramesh23 <golu677388@gmail.com>
2026-08-19 13:16:51 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// ===
+
+console.log("2"===2);//strict comparison
+
+// line 11 to 17 these are not a good practice for comparison 
